9528d4c232ac12dfb5309b3e73b5d2dcb975a257
shripada <shripada.hebbar@codecraft.co.in>
2024-01-03 14:23:46 +0530

    fibs without use memo

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
 import { CounterDisplay } from './CounterDisplay';
 
+function firstFibs(fibCount: number) {
+  let f1 = 0,
+    f2 = 1;
+  if (fibCount === 0) {
+    return [];
+  }
+  if (fibCount === 1) {
+    return [f1];
+  }
+  if (fibCount === 2) {
+    return [f1, f2];
+  }
+
+  const fibs = [];
+  for (let i = 2; i <= fibCount; i++) {
+    fibs[i] = f2 + f1;
+    f1 = f2;
+    f2 = fibs[i];
+  }
+  return fibs;
+}
+
 const Counter = () => {
   const [count, setCount] = React.useState(0);
+  const [fibCount, setFibCount] = React.useState(0);
+  const inputId = React.useId();
+
+  function handleSubmit(e: {
+    preventDefault: () => void;
+    currentTarget: HTMLFormElement | undefined;
+  }) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const formObj = Object.fromEntries(formData);
+    setFibCount(new Number(formObj['fibCount']).valueOf());
+  }
+
+  const fibs = firstFibs(fibCount);
 
   return (
     <div style={{ border: '2px solid hsl(26, 100%, 50%, 1)', padding: '10px' }}>
-      <CounterDisplay count={count} />
+      <CounterDisplay count={count} fibs={fibs} />
       <button
         onClick={() => {
           setCount(count + 1);
@@ -14,6 +50,13 @@ const Counter = () => {
       >
         ➕
       </button>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <label htmlFor={inputId}> Number of fibs:</label>
+          <input defaultValue={0} name="fibCount" type="number" id={inputId} />
+        </fieldset>
+        <button>Go</button>
+      </form>
     </div>
   );
 };
--- src/CounterDisplay.tsx
+++ src/CounterDisplay.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ReactLogo } from './ReactLogo';
 
-const CounterDisplay = ({ count }: { count: number }) => {
+const CounterDisplay = ({ count, fibs }: { count: number; fibs: number[] }) => {
   console.info('CounterDisplay render');
 
   const listColor = count % 4 === 0 ? 'red' : 'green';
@@ -15,12 +15,21 @@ const CounterDisplay = ({ count }: { count: number }) => {
       style={{
         border: '2px solid hsla(214, 100%, 50%, 1)',
         borderRadius: '10px',
+        margin: '15px',
       }}
     >
       <h1>Count value</h1>
       <h2>{count}</h2>
 
       <ReactLogo {...reactLogoProps} />
+      <h4>Fibs:</h4>
+      <div style={{ height: '140px', overflow: 'scroll', textAlign: 'left' }}>
+        <ol>
+          {fibs.map((fib, index) => (
+            <li key={index}>{fib}</li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
