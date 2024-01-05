bbc4b22f4624197ec19be234cd11f763f488e196
shripada <shripada.hebbar@codecraft.co.in>
2024-01-05 14:59:16 +0530

    UserBanner with prop delegation

--- src/UserBanner.tsx
+++ src/UserBanner.tsx
@@ -1,21 +1,20 @@
-import { ReactNode } from 'react';
+import { ComponentProps } from 'react';
 import Banner from './Banner';
 
-function UserBanner({
-  user,
-  type,
-  children,
-}: {
-  user: { registrationStatus: string };
-  type: string;
-  children: ReactNode;
-}) {
+// ComponentProps is a nice technique to extract props of a component.
+// Refer: https://www.totaltypescript.com/react-component-props-type-helper
+type BannerProps = ComponentProps<typeof Banner>;
+type UserBannerProps = BannerProps & { user: { registrationStatus: string } };
+
+// Delegating props that the composing component does not use, but
+// needs to pass them down to the composed component.
+function UserBanner({ user, ...delegated }: UserBannerProps) {
   // Only logged in, verified users are
   // allowed to see the banner
   if (!user || user.registrationStatus === 'unverified') {
     return null;
   }
-  return <Banner type={type} children={children} />;
+  return <Banner {...delegated} />;
 }
 
 export default UserBanner;
